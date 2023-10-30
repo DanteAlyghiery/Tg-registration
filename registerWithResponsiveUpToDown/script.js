8bd49c0e14d718012c49776f6397733587b35a5d
DanteAlyghiery <69762288+DanteAlyghiery@users.noreply.github.com>
2023-10-30 20:47:23 +0200

    listenersJsforInputs

--- registerWithResponsiveUpToDown/script.js
+++ registerWithResponsiveUpToDown/script.js
@@ -11,3 +11,18 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active")
 });
+
+
+let tg = window.Telegram.WebApp;
+let buy = document.getElementById('buy');
+let order = document.getElementById('order');
+
+buy.addEventListener('click', () => {
+    document.getElementById('main').style.display = 'none';
+    document.getElementById('form').style.display = 'block';
+    document.getElementById('user_name').value = tg.initDataUnsafe.user.first_name + '' + tg.initDataUnsafe.user.last_name;
+
+    order.addEventListener('click', () => {
+        tg.close();
+    })
+})
